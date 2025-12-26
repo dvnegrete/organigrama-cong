557d2715d7573b6f360ea04b4bab2eafb6fd5d21
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2025-12-26 12:08:44 -0600

    feat: Añadir configuración del servidor en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/organigrama-cong/'
+  base: '/organigrama-cong/',
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: false,
+    allowedHosts: 'all'
+  }
 })
